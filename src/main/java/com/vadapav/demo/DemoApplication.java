63af380bcdb9327a70604cb3fbbec9765eaf5e50
haripriya <rm.haripriya@gmail.com>
2023-04-23 15:14:44 +0530

    get metadata

--- src/main/java/com/vadapav/demo/DemoApplication.java
+++ src/main/java/com/vadapav/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.vadapav.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/api/platform")
+    String platform() {
+        return System.getProperty("java.vendor") + " @ " + System.getProperty("java.version");
+    }
+
+	@RequestMapping("/api/foundation")
+	String foundation() {
+		return "Spring Boot @ " + SpringBootVersion.getVersion();
 	}
 
 }
